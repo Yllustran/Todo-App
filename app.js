5ea20ac82feae8d59670b0899cff5cedfc260873
Yllustran <sullivantra@gmail.com>
2024-07-18 11:14:48 +0200

    - Passage des todos en objets - Ajout de la propriété completed aux tâches - Ajout feature checkbox pour marquer les tâches comme faites - Correction de la syntaxe de innerHTML - Sauvegarde de l'état fait/non fait des tâches - Refonte des commentaire du code app.js

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-// Sélectionne les éléments du DOM nécessaires pour la gestion de la liste de tâches
-const todoForm = document.querySelector('form'); // Sélectionne le formulaire des tâches
-const todoInput = document.getElementById('todo-input'); // Sélectionne le champ de saisie des nouvelles tâches
-const todoListUL = document.getElementById('todo-list'); // Sélectionne l'élément UL où les tâches seront affichées
+// Sélection des éléments du DOM pour la gestion de la liste de tâches
+const todoForm = document.querySelector('form'); // Formulaire des tâches
+const todoInput = document.getElementById('todo-input'); // Champ de saisie des nouvelles tâches
+const todoListUL = document.getElementById('todo-list'); // Élément UL où les tâches seront affichées
 
-// Initialise la liste des tâches en récupérant les tâches sauvegardées depuis le stockage local
+// Initialisation de la liste des tâches en récupérant les tâches sauvegardées depuis le stockage local
 let allTodos = getTodos();
-// Met à jour l'affichage de la liste des tâches
+// Mise à jour de l'affichage de la liste des tâches
 updateTodoList();
 
-// Ajoute un gestionnaire d'événements pour l'envoi du formulaire
+// Ajout d'un gestionnaire d'événements pour l'envoi du formulaire
 todoForm.addEventListener('submit', function(e) {
-    e.preventDefault(); // Empêche le comportement par défaut du formulaire (rechargement de la page)
+    e.preventDefault(); // Empêche le rechargement de la page
     addTodo(); // Ajoute une nouvelle tâche
 });
 
 // Fonction pour ajouter une nouvelle tâche
 function addTodo(){
-    const todoText = todoInput.value.trim(); // Récupère et nettoie la saisie de l'utilisateur
+    const todoText = todoInput.value.trim(); // Nettoie la saisie de l'utilisateur
     if (todoText.length > 0){
-        allTodos.push(todoText); // Ajoute la tâche à la liste
-        updateTodoList(); // Met à jour l'affichage de la liste des tâches
-        saveTodos(); // Sauvegarde les tâches dans le stockage local
+        const todoObject = {
+            text: todoText,
+            completed: false
+        }
+        allTodos.push(todoObject); // Ajoute la tâche à la liste
+        updateTodoList(); // Met à jour l'affichage
+        saveTodos(); // Sauvegarde les tâches
         todoInput.value = ""; // Vide le champ de saisie
     }
 }
@@ -28,7 +32,7 @@ function addTodo(){
 // Fonction pour mettre à jour l'affichage de la liste des tâches
 function updateTodoList(){
     todoListUL.innerHTML = ""; // Vide l'élément UL
-    allTodos.forEach((todo, todoIndex) => { // Parcourt chaque tâche de la liste
+    allTodos.forEach((todo, todoIndex) => { // Parcourt chaque tâche
         const todoItem = createTodoItem(todo, todoIndex); // Crée un élément de tâche
         todoListUL.append(todoItem); // Ajoute l'élément de tâche à l'UL
     });
@@ -36,9 +40,10 @@ function updateTodoList(){
 
 // Fonction pour créer un élément de tâche
 function createTodoItem(todo, todoIndex){
-    const todoId = "todo-" + todoIndex; // Génère un identifiant unique pour la tâche
+    const todoId = "todo-" + todoIndex; // Génère un identifiant unique
     const todoLI = document.createElement("li"); // Crée un élément LI
-    todoLI.className = "todo"; // Ajoute une classe CSS à l'élément LI
+    const todoText = todo.text;
+    todoLI.className = "todo"; // Ajoute une classe CSS
     todoLI.innerHTML = 
     `<input type="checkbox" id="${todoId}">
                 <label class="custom-checkbox" for="${todoId}">
@@ -46,35 +51,41 @@ function createTodoItem(todo, todoIndex){
                         <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
                     </svg>
                 </label>
-                <label for="${todoId}" class="todo-text"> ${todo} </label>
+                <label for="${todoId}" class="todo-text"> ${todoText} </label>
                 <button class="delete-button">
                     <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                         <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
                     </svg>
                 </button>`;
 
-    const deleteButton = todoLI.querySelector(".delete-button"); // Sélectionne le bouton de suppression de la tâche
+    const deleteButton = todoLI.querySelector(".delete-button"); // Sélectionne le bouton de suppression
     deleteButton.addEventListener("click", () => {
-        deleteTodoItem(todoIndex); // Ajoute un gestionnaire d'événements pour supprimer la tâche lorsqu'on clique sur le bouton
-    })
+        deleteTodoItem(todoIndex); // Supprime la tâche au clic
+    });
+    const checkbox = todoLI.querySelector('input');
+    checkbox.addEventListener("change", () => { 
+        allTodos[todoIndex].completed = checkbox.checked; // Met à jour l'état de complétion
+        saveTodos(); // Sauvegarde les tâches
+    });
+    checkbox.checked = todo.completed; // Initialise l'état du checkbox
     return todoLI; // Retourne l'élément de tâche
 }
 
 // Fonction pour supprimer une tâche de la liste
 function deleteTodoItem(todoIndex) {
-    allTodos = allTodos.filter((_, i) => i !== todoIndex); // Filtre la liste pour exclure la tâche à supprimer
-    saveTodos(); // Sauvegarde la liste mise à jour dans le stockage local
-    updateTodoList(); // Met à jour l'affichage de la liste des tâches
+    allTodos = allTodos.filter((_, i) => i !== todoIndex); // Exclut la tâche à supprimer
+    saveTodos(); // Sauvegarde la liste mise à jour
+    updateTodoList(); // Met à jour l'affichage
 }
 
 // Fonction pour sauvegarder les tâches dans le stockage local
 function saveTodos() {
-    const todosJson = JSON.stringify(allTodos); // Convertit la liste des tâches en JSON
-    localStorage.setItem("todos", todosJson); // Sauvegarde les tâches dans le stockage local
+    const todosJson = JSON.stringify(allTodos); // Convertit la liste en JSON
+    localStorage.setItem("todos", todosJson); // Sauvegarde dans le stockage local
 }
 
 // Fonction pour récupérer les tâches depuis le stockage local
 function getTodos(){
-    const todos = localStorage.getItem("todos") || "[]"; // Récupère les tâches ou une liste vide si aucune tâche n'est sauvegardée
+    const todos = localStorage.getItem("todos") || "[]"; // Récupère les tâches ou une liste vide
     return JSON.parse(todos); // Convertit le JSON en objet JavaScript
 }
