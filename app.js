2acb02e8d426afbb55ce9fe276f9c3c0694884a8
Yllustran <sullivantra@gmail.com>
2024-07-18 10:02:34 +0200

    - Ajout de la feature : Local storage de la Todo List

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
 
-let allTodos = [];
+let allTodos = getTodos();
+updateTodoList();
 
 todoForm.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -14,6 +15,7 @@ function addTodo(){
     if (todoText.length > 0){
         allTodos.push(todoText);
         updateTodoList();
+        saveTodos();
         todoInput.value = "";
     }
 }
@@ -45,3 +47,14 @@ function createTodoItem(todo, todoIndex){
                 </button>`;
     return todoLI;
 }
+
+function saveTodos() {
+    const todosJson = JSON.stringify(allTodos);
+    localStorage.setItem("todos", todosJson);
+}
+
+function getTodos(){
+    const todos = localStorage.getItem("todos") || "[]";
+    return JSON.parse(todos);
+}
+
